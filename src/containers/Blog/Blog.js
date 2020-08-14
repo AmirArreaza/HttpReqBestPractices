8a466bb3978f16d2e0724353312f9864022d02d4
AmirArreaza <ajaarreazav@gmail.com>
2020-08-14 15:01:14 +0100

    Added the NewPost route to the Blog

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 
 import "./Blog.css";
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost'
 
 class Blog extends Component {
   render() {
@@ -22,6 +23,7 @@ class Blog extends Component {
         </header>
         {/*<Route path="/" exact render={() => <Posts />}/>*/}
         <Route path="/" exact component={Posts}/>
+        <Route path="/new-post" exact component={NewPost}/>
       </div>
     );
   }
